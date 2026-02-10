0ba07198d2cfece187739e26c3dc5fe3979bdb9c
likeyeon <chy98csy00@naver.com>
2026-02-10 20:34:24 +0900

    ✨ Implement project item sliding on PC view

--- js/project-slide.js
+++ js/project-slide.js
@@ -1,5 +1,15 @@
 const projectHeadList = document.querySelectorAll('.project-head-item');
 const projectTailList = document.querySelectorAll('.project-tail-item');
+const projectPcPrevBtn = document.querySelector(
+  '.project-content-pc-wrapper .swiper-button-prev',
+);
+const projectPcNextBtn = document.querySelector(
+  '.project-content-pc-wrapper .swiper-button-next',
+);
+const projectItemLength = projectHeadList.length;
+const VISIBLE_COUNT = 3;
+let currentLastSlideNumber = 3;
+let currentProjectNavigation = 0;
 
 projectHeadList.forEach((item, idx) => {
   item.addEventListener('mouseenter', function () {
@@ -18,8 +28,8 @@ new Swiper('.project .swiper', {
   slidesOffsetBefore: 15,
   slidesOffsetAfter: 15,
   navigation: {
-    nextEl: '.project .swiper-button-next',
-    prevEl: '.project .swiper-button-prev',
+    nextEl: '.project-content-mo .swiper-button-next',
+    prevEl: '.project-content-mo .swiper-button-prev',
   },
   breakpoints: {
     768: {
@@ -28,3 +38,39 @@ new Swiper('.project .swiper', {
     },
   },
 });
+
+/* PC - 슬라이드 이동 */
+function calculateProjectItemLeft(elementList) {
+  elementList.forEach((item, idx) => {
+    item.style.setProperty(
+      'left',
+      ((100 / VISIBLE_COUNT) * (idx - currentProjectNavigation)).toFixed(2) +
+        '%',
+    );
+  });
+}
+
+function updateProjectButtonState() {
+  projectPcPrevBtn.classList.toggle(
+    'swiper-button-disabled',
+    currentLastSlideNumber === VISIBLE_COUNT,
+  );
+
+  projectPcNextBtn.classList.toggle(
+    'swiper-button-disabled',
+    currentLastSlideNumber === projectItemLength,
+  );
+}
+
+function moveProjectSlide(direction) {
+  currentProjectNavigation += direction;
+  currentLastSlideNumber += direction;
+
+  calculateProjectItemLeft(projectHeadList);
+  calculateProjectItemLeft(projectTailList);
+
+  updateProjectButtonState();
+}
+
+projectPcPrevBtn.addEventListener('click', () => moveProjectSlide(-1));
+projectPcNextBtn.addEventListener('click', () => moveProjectSlide(1));
